dc587b012f394950995718a91333c6dafef63cd4
meloalright <meloalright@gmail.com>
2023-03-20 00:58:51 +0800

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+// in lib.rs
+pub mod first;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+fn main() {
+    
+}
